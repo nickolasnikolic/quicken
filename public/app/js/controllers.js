300651fc8bf8e0c3924ad28dd6be645bf5368a72
Nickolas Nikolic <subclass@gmail.com>
2015-10-22 17:39:21 -0500

    git er done

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -1,7 +1,6 @@
 blindApp.controller('IndexController', ['$scope', '$state', 'globals', function($scope, $state, globals) {}])
 
 blindApp.controller('HomeController', ['$scope', '$state', '$http', '$sce', 'globals', function($scope, $state, $http, $sce, globals) {
-  document.title = 'blindworm - home'; //set the page title
 
   $scope.getItems = function( keywords ){
       $http.get('/api/az/' + keywords)
