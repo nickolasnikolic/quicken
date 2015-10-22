d79a82111031c713314e4ccb51c7a9933525250d
Nickolas Nikolic <subclass@gmail.com>
2015-10-22 16:56:16 -0500

    git er done

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -6,7 +6,7 @@ blindApp.controller('HomeController', ['$scope', '$state', '$http', '$sce', 'glo
   $scope.getItems = function( keywords ){
       $http.get('/api/az/' + keywords)
           .then(function(response){
-            console.log(response.data);
+            console.log(response.data.ItemSearchResponse);
             $scope.items = response.data.ItemSearchResponse.Items;
           });
   };
